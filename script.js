7c3a00e7c135f271eb4b85c54aa93df6d239a3a1
Alkida Vaci <alkida.vaci@gmail.com>
2022-10-10 22:16:21 -0400

    Series of prompt and confirms for password criteria

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Series of prompt and confirms for password criteria
+var inputLength = window.prompt("How long do you want your password?", "12");
+var inputIncludesNumbers = window.confirm("Would you like to include numbers to your password?")
+var inputIncludesLowercase = window.confirm("Would you like to include Lowercase to your password?")
+var inputIncludesUppercase = window.confirm("Would you like to include Uppercase to your password?")
+var inputIncludesSpecialCharacter = window.confirm("Would you like to include SpecialCharacter to your password?")
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
