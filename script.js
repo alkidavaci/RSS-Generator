a01f5b36232a07d331b8c9c266d5854ce7f2e5bd
Alkida Vaci <alkida.vaci@gmail.com>
2022-10-10 22:24:05 -0400

    Added Alerts so the input is number,more than 8 less than 128 characters

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Series of prompt and confirms for password criteria
 var inputLength = window.prompt("How long do you want your password?", "12");
+
+ //Added Alerts so the input is number,more than 8 less than 128 characters
+ var passwordLength = parseInt(inputLength);
+
+ if (isNaN(passwordLength)) {
+   window.alert("Enter only number!");
+   return;
+ } else if (passwordLength < 8) {
+   window.alert("Password is to short! Mu");
+ } else if (passwordLength > 128) {
+   window.alert("Password is to long!");
+ } else {
+   window.alert("This is a valid number");
+ }
+
 var inputIncludesNumbers = window.confirm("Would you like to include numbers to your password?")
 var inputIncludesLowercase = window.confirm("Would you like to include Lowercase to your password?")
 var inputIncludesUppercase = window.confirm("Would you like to include Uppercase to your password?")
